83bd6a8b0f71874fd6cd33d84370ddfa66c716f7
javier nuñez <javier@MacBook-Pro-de-javier.local>
2021-12-23 12:38:56 -0500

    c

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 var pgp = require("pg-promise")(/*options*/);
 const conf = {
-  host: "localhost", // server name or IP address;
+  host: "ec2-3-217-170-198.compute-1.amazonaws.com", // server name or IP address;
   port: 5432, // default port number for PostgreSQL
-  database: "test", // database name
-  user: "postgres", // user name
-  password: "salomeluna", // password
+  database: "de5001mav3h8q7", // database name
+  user: "zmzcrlvamxduyl", // user name
+  password: "5097ae04a5d1d1e4ba9826baf1c58233ee4c5908209505f7697a37f7864c115a", // password
 };
 const db = pgp(
   `postgres://${conf.user}:${conf.password}@${conf.host}:${conf.port}/${conf.database}`
